c6b980aec7a653953ca38516f81defebe76e9dfc
Jacqueline Russell <jacqueline.russell@microsoft.com>
2021-05-24 11:27:02 -0700

    Added v2 blocks

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 let index = 0
+input.onButtonPressed(Button.A, function () {
+    soundExpression.happy.playUntilDone()
+})
 function fibonacci (n: number): any {
     if (n <= 2) {
         return Math.min(n, 1)
     }
     return fibonacci(n - 1) + fibonacci(n - 2)
 }
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.soundLevel())
+})
 basic.forever(function () {
     index += 1
     basic.clearScreen()
